e439651f2d19613fec8eb72fa320636037b89ea6
Janashell <janashell07@gmail.com>
2021-02-22 12:22:07 -0600

    JS Basics Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,29 @@
+// 1
+console.log(7);
+
+// 2
+// Numbers, Strings, Booleans, Null, and Undefined
+
+// 3
+// +, -, *, /, **, %
+// Addition, Subtraction, Multiplication, Division, Exponentiation, and Modulus
+
+// 4
+console.log(5%2);
+console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("Wanda " + "Vision");
+
+// 7
+console.log("Jana".length + "Shell".length);
+
+// 8
+console.log("Avengers:Endgame"[15]);
+
+// BONUS
+
+// 1
